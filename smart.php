d9dd10fc00d25010a77b51456f759112d156be21
Gaurav <gaurav@smartcommercesolutions.in>
2018-11-17 15:43:24 +0530

    testing with php code

---
+++ smart.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+function processMessage($update) {
+    if($update["result"]["action"] == ""){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => $update["result"]["parameters"]['echoText'],
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
+    }
+}
+
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
+?>
